ac8216b023eb357500ef3ab11dcf1f6239689ff3
Mark Stanislawek <mark.stanislawek@gmail.com>
2017-08-03 20:43:14 -0500

    initial project bootstrap

---
+++ src/main/java/mannafundraising/orderforms/OrderformsApplication.java
@@ -0,0 +1,12 @@
+package mannafundraising.orderforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderformsApplication.class, args);
+	}
+}
---
+++ src/test/java/mannafundraising/orderforms/OrderformsApplicationTests.java
@@ -0,0 +1,16 @@
+package mannafundraising.orderforms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderformsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
